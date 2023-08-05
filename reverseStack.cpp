06c371c42f78d8ba61aa9d4b1c2bb99eeb4b961d
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-06 01:42:50 +0530

    Create reverseStack.cpp

---
+++ reverseStack.cpp
@@ -0,0 +1,74 @@
+/*gfg link :- https://practice.geeksforgeeks.org/problems/reverse-a-stack/1
+You are given a stack St. You have to reverse the stack using recursion.
+
+Example 1:
+
+Input:
+St = {3,2,1,7,6}
+Output:
+{6,7,1,2,3}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    void Reverse(stack<int> &st){
+        
+        int x = st.size();
+        //base case
+        if(x<=0){
+            return;
+        }
+        
+        cout<<st.top()<<" ";
+        st.pop();
+        x--;
+        
+        Reverse(st); // recursive call
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
