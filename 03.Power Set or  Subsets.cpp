c1b82f8a63db1a6fe2ea6f7485850c40f62dc708
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-05 22:35:06 +0530

    Update 03.Power Set or  Subsets.cpp

--- 03.Power Set or  Subsets.cpp
+++ 03.Power Set or  Subsets.cpp
@@ -1,4 +1,4 @@
-Leetcode:-
+/*Leetcode:-
 Given an integer array nums of unique elements, return all possible 
 subsets
  (the power set).
@@ -9,7 +9,7 @@ The solution set must not contain duplicate subsets. Return the solution in any 
 
 Input: nums = [1,2,3]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-
+*/
 
 class Solution {
 public:
@@ -28,7 +28,7 @@ public:
         //ith elememnt is included in subset
         subset.push_back(nums[i]);
         generate(subset,i+1,nums);
-        subset.pop_back();
+        subset.pop_back(); //backtracking
 
 
     }
