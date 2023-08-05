ffb0420a77e129fa259b8385006fd8561452af65
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-05 10:23:17 +0530

    Create combinationpattern.cpp

---
+++ combinationpattern.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+/* pattern one
+1 2 3 4 5.....
+1 2 3 4 
+1 2 3 
+1 2 
+1  ->> imp base case here do check 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 ......
+*/
+
+void patternPrint(int n)
+{
+	//base case
+	//if(n==0) return; //this condition prints 1 twice but we need it only once
+    
+    if(n==1)
+    {
+    	cout<<1<<"\n"; //if not printed 1 then 1 gets skipped
+    	return;
+    }
+
+   
+	for(int i = 1 ;i<=n;i++){ // print first line by urself
+		cout<<i<<" ";
+	}
+	cout<<"\n";
+
+	patternPrint(n-1); //Trust and call func for middle part
+
+	for(int i = 1 ;i<=n;i++){ // print last line by urself
+		cout<<i<<" ";
+	}
+	cout<<"\n";
+}
+
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	patternPrint(n);
+	
+}
