6fbbc2cc53100cf42e8b6ecf2bf9c972bfc4a7e2
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-12 14:54:25 +0530

    Update and rename generate parenthesis to 10.generate parenthesis.cpp

--- 10.generate parenthesis.cpp
+++ 10.generate parenthesis.cpp
@@ -1,12 +1,12 @@
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-Example 1:
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// Example 1:
 
-Input: n = 3
-Output: ["((()))","(()())","(())()","()(())","()()()"]
-Example 2:
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
 
-Input: n = 1
-Output: ["()"]
+// Input: n = 1
+// Output: ["()"]
 
 class Solution {
 public:
