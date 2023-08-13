329583c5a8e1d0178c0f066fdd2121de087f880b
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-13 12:31:21 +0530

    Update 02.UniquePermutations.cpp

--- 02.UniquePermutations.cpp
+++ 02.UniquePermutations.cpp
@@ -9,6 +9,7 @@
 // Example 2:
 // Input: nums = [1,2,3]
 // Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// Total number of permutations = n! where n is length of string
 
 
 class Solution {
@@ -23,7 +24,7 @@ public:
         unordered_set<int> s1; // set is removes the duplicate elements
         
         for (int i = pos; i < s.size(); i++) {
-            if(s1.find(s[i])!= s1.end()) continue; //skip duplicates
+            if(s1.find(s[i])!= s1.end()) continue; // it means a duplicate is found so skip duplicates
             s1.insert(s[i]); // insert into set so that no duplicate value occurs
             swap(s[i], s[pos]);
             solve(s, ans, pos + 1);
