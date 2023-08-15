63bdf18cbd1ae735b92d0fd3a41f4581827ccc72
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-15 11:09:18 +0530

    Update 20.(LCS)Longest Common Subsequence.cpp

--- 20.(LCS)Longest Common Subsequence.cpp
+++ 20.(LCS)Longest Common Subsequence.cpp
@@ -1,1 +1,20 @@
-/**/
+/*
+Given two strings, S1 and S2, the task is to find the length of the Longest Common Subsequence, i.e. longest subsequence present in both of the strings. 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns length of LCS for X[0..m-1], Y[0..n-1]
+int lcs(string X, string Y, int m, int n)
+{
+	//base case 
+  if (m == 0 || n == 0)
+		return 0;
+  //check last character oif both strings
+	if (X[m - 1] == Y[n - 1])
+		return 1 + lcs(X, Y, m - 1, n - 1);
+	else
+		return max(lcs(X, Y, m, n - 1),
+				lcs(X, Y, m - 1, n));
+}
